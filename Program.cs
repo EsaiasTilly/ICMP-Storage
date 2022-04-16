04c675b3a13f89c1d7dc87b9713f5600a34c22d6
Esaias Tilly <esaias@tillys.se>
2022-04-16 19:36:00 +0200

    Send and recieve buffer

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+var ping = new Ping();
+var reply = ping.Send("1.1.1.1", 10000, Encoding.UTF8.GetBytes("Hello World"));
+String buffer = Encoding.UTF8.GetString(reply.Buffer);
+Console.WriteLine(buffer);
